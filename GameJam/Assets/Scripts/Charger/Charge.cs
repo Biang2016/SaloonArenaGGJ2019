ec00a8cec5f2029d2156d6a61e697dc9380d7b0b
Medemus <39671548+Medemus@users.noreply.github.com>
2019-01-26 15:16:02 +0800

    u

---
+++ GameJam/Assets/Scripts/Charger/Charge.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Charge : MonoBehaviour
+{
+    public float speed;
+    public PlayerBody p;
+    public int whichPlayer;
+    bool Charging;
+
+    private void FixedUpdate()
+    {
+        if (Charging)
+            p.Add_Energy(speed * Time.deltaTime);
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (((int)collision.GetComponent<PlayerBody>().WhichPlayer+1) == whichPlayer)
+        {
+            Charging = true;
+            if (p == null)
+                p = collision.gameObject.GetComponent<PlayerBody>();
+        }
+           
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (((int)collision.GetComponent<PlayerBody>().WhichPlayer + 1) == whichPlayer)
+            Charging = false;
+    }
+
+}
