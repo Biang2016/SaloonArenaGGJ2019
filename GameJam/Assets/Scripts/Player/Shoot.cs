5f9dce861886f6df56f80c574ce47c11b12f5af6
Biang <biang@BiangdeMacBook-Pro.local>
2019-01-26 18:37:09 +0800

    u

--- GameJam/Assets/Scripts/Player/Shoot.cs
+++ GameJam/Assets/Scripts/Player/Shoot.cs
@@ -8,6 +8,7 @@ public class Shoot : MonoBehaviour
     public Transform shoot_point;
     public float shoot_speed;
     public GameObject ammo;
+
     private void FixedUpdate()
     {
         if (Input.GetButtonDown(PlayerBody.Index_name + "fire"))
@@ -25,12 +26,9 @@ public class Shoot : MonoBehaviour
         int sfx_index = Random.Range(0, 2);
         PlayerBody.SoundPlay("sfx/HitFromFar_" + sfx_index, 0.5f);
         Ammo am = GameObjectPoolManager.Instance.Pool_GarbageLitter.AllocateGameObject<Ammo>(GameBoardManager.Instance.GameBoardGarbagesCanvas.transform);
-        Instantiate(ammo, shoot_point.position,Quaternion.Euler(dir)).GetComponent<Ammo>();
-        
-        am.player = (int)PlayerBody.WhichPlayer;
-        //am.CanPick = false;
+        am.player = (int) PlayerBody.WhichPlayer;
         am.transform.position = shoot_point.transform.position;
-        am.transform.rotation = transform.rotation;
+        am.transform.rotation = shoot_point.transform.rotation;
         am.Rigidbody2D.velocity = dir * shoot_speed;
     }
 }
