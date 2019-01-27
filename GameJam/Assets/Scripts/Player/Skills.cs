9d0c86e301367d8ca544f3b13e0a554470480381
Biang <biang@BiangdeMacBook-Pro.local>
2019-01-27 08:32:14 +0800

    u

---
+++ GameJam/Assets/Scripts/Player/Skills.cs
@@ -0,0 +1,17 @@
+﻿public abstract class Skills
+{
+    public enum SkillType
+    {
+        TimeStop,
+        BaseballBar,
+        Pitch,
+        SubRobot,
+        RotateSelf,
+        Rush,
+    }
+
+    public SkillType M_SkillType;
+    public PlayerBody PlayerBody;
+
+    public abstract void Skill_Execute();
+}
